ebc43f6a64694ccaf345a6058b1164390b49f79a
mathew <mathew@codeperate.com>
2025-01-28 17:15:17 +0000

    export builder config

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './define-entity-schemas.js';
 export * from './entity-schema-builder.js';
 export * from './use-schema.js';
 export * from './schema-storage.js';
+export * from './builder-config.js';
